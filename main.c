e3059a72c6035e761920317cd5ab63509f55ba75
Scott Bennett <scottb@fastmail.com>
2017-03-22 22:06:43 -0400

    Add missing include, more for correctness than anything

--- main.c
+++ main.c
@@ -13,6 +13,8 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#include <sys/types.h>
+
 #include <err.h>
 #include <limits.h>
 #include <stdio.h>
