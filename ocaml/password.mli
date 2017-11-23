fe747eebe7bc9d92a92cd46a348cecbd57403bad
Scott Bennett <sbennett@gpm.md>
2017-11-23 09:51:53 -0500

    Take out module stuff and the abstract type

--- ocaml/password.mli
+++ ocaml/password.mli
@@ -1,10 +1,7 @@
-(** An ASCII character password *)
-type t
-
 (**
  * Create a password of specified length, filled with random ASCII characters
  *)
-val generate_password : int -> t
+val generate_password : int -> char list
 
 (** Print the password to stdout, followed by a newline *)
-val print_password : t -> ()
+val print_password : char list -> ()
